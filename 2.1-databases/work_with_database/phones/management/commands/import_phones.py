8363591f6b37b45adfd5516cc63c0bf2be2e12e0
alllianka <alllianka@icloud.com>
2026-02-28 22:08:56 +0300

    Update import_phones.py

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -14,4 +14,13 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            slug = phone['name'].lower().replace(' ', '_')
+            item = Phone(
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=slug,                
+                )
+            item.save()
