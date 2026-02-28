21aaae19b90615ac48ebf05ebd56d02cbc373698
alllianka <alllianka@icloud.com>
2026-02-28 22:00:23 +0300

    Update views.py

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+
+from books.models import Book
+
+
+def index_view(request):
+    return redirect('books')
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    data = Book.objects.all()
+    context = {
+        'books': data,
+    }
+    return render(request, template, context)
+
+def books_date_view(request, date):
+    template = 'books/books_list.html'
+    data = Book.objects.all().filter(pub_date=date)
+    page = Book.objects.values('pub_date')
+    context = {
+        'books': data,
+        'pages': page,
+    }
     return render(request, template, context)
