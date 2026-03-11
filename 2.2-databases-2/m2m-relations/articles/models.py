150e234aa8ae5d6b80887cc3dcb20608d5e63c53
alllianka <alllianka@icloud.com>
2026-03-11 14:12:32 +0300

    Update models.py

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
 
+class Tag(models.Model):
+
+    name = models.CharField(max_length=16, verbose_name='Тэг')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
 
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +26,14 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scopes(models.Model):
+
+    tag = models.ForeignKey(Tag, related_name='scopes', on_delete=models.CASCADE, verbose_name='Раздел')
+    article = models.ForeignKey(Article, related_name='scopes', on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
