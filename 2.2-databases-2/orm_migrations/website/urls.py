ea8845d4832a039106796f3407983e33d07bb17e
alllianka <alllianka@icloud.com>
2026-03-11 14:06:48 +0300

    Update urls.py

--- 2.2-databases-2/orm_migrations/website/urls.py
+++ 2.2-databases-2/orm_migrations/website/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('__debug__/', include('debug_toolbar.urls')),
     path('', include('school.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
